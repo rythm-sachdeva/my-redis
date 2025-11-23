e47f12176d6a0fe35fd2921e21252bcac14b51c5
Rythm Sachdeva <rythmsachdeva@MacBook-Pro.local>
2025-11-23 18:07:22 +0530

    feat: Added a DB with mutex locks

---
+++ src/db/db.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use std::sync::{Arc,Mutex};
+use bytes::Bytes;
+
+
+#[derive(Clone)]
+pub struct Db{
+    shared: Arc<Mutex<HashMap<String,Bytes>>>
+}
+
+impl Db {
+    pub fn new() ->Db{
+        Db {
+            shared : Arc::new(Mutex::new(HashMap::new()))
+        }
+    }
+
+    pub fn set(&self,key:String,bytes:Bytes) {
+        let mut state = self.shared.lock().unwrap();
+        state.insert(key, value);
+    }
+
+    pub fn get(&self,key:String) -> Option<Bytes>{
+       let state = self.shared.lock().unwrap();
+       state.get(key).cloned()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use db::Db;
+use tokio::io::{AsyncReadExt,AsyncWriteExt};
+use tokio::net::{TcpListener,TcpStream};
+use bytes::BytesMut;
+
+
+#[tokio::main]
+async fn main(){
+     
+     let listener = TcpListener::bind("127.0.0.1:6379").await?;
+     let db = Db::new();
+    
+
+     loop {
+
+      let (socket,_) = listener.accept().await?;   
+      let db = db.clone();
+
+
+     }
+
+
+
 }
